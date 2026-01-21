95c74a30a6c2f77d4167733fbd839e36131fcc76
MrDyslexia <mhhernand@gmail.com>
2026-01-21 01:45:05 -0300

    listo a produccion(?

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   // Allow remote origin to load dev assets until Next.js enforces explicit allowlist
   allowedDevOrigins: ['https://m4.blocktype.cl','m4.blocktype.cl','http://m4.blocktype.cl'],
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
